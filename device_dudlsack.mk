a8da190212b4a79371b51ccb841e4c36cca29de6
Christoph H44z <christoph.h@sprinternet.at>
2016-02-28 15:42:28 +0000

    added some libs

--- device_dudlsack.mk
+++ device_dudlsack.mk
@@ -17,7 +17,20 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/system/bin/linker:recovery/root/system/bin/linker \
+    $(LOCAL_PATH)/recovery/system/lib/libc.so:recovery/root/system/lib/libc.so \
+    $(LOCAL_PATH)/recovery/system/lib/libdl.so:recovery/root/system/lib/libdl.so \
+    $(LOCAL_PATH)/recovery/system/lib/liblog.so:recovery/root/system/lib/liblog.so \
+    $(LOCAL_PATH)/recovery/system/lib/libm.so:recovery/root/system/lib/libm.so \
+    $(LOCAL_PATH)/recovery/system/lib/libstdc++.so:recovery/root/system/lib/libstdc++.so \
+    $(LOCAL_PATH)/recovery/system/lib/libz.so:recovery/root/system/lib/libz.so
+
 $(call inherit-product, build/target/product/full.mk)
+
+# OpenGL ES
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb 
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_dudlsack
